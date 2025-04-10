243d330db6759d552a281043eb8f06a7a01ab703
itsnaved <itsnaved11@gmail.com>
2025-04-10 18:04:30 +0530

    Remove All Whitespaces From a String

---
+++ removeWhitspaces.js
@@ -0,0 +1,10 @@
+const str = "             Javascript is most popular programming language ";
+
+const result = str.split(" ").join("");
+console.log(result);
+
+
+
+const text = "Javascript is an interpreted language";
+const newText= text.replace(/\s/g,"");
+console.log(newText);
