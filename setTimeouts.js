9a0abac68a449b8cf61dc26dac735c7d1f2ffb88
itsnaved <itsnaved11@gmail.com>
2025-04-10 15:20:14 +0530

    setTimeout and setInterval Functions

---
+++ setTimeouts.js
@@ -0,0 +1,11 @@
+function about(){
+    console.log("JavaScript is the worlds most popular programming language. JavaScript is the programming language of the Web.");
+}
+
+function intervals(){
+    console.log("Set interval Execution");
+}
+
+setInterval(intervals,3000);
+setTimeout(about, 2000);
+console.log("Execution");
